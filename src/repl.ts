3a537206e6d41f18db62b75ef5f869d80001baa1
mchin24 <mchin24@gmail.com>
2026-05-06 11:39:32 -0400

    Added repl interface - App now prompts for input, processes, and displays output on loop - CTRL-C to break out

--- src/repl.ts
+++ src/repl.ts
@@ -2,3 +2,25 @@ export function cleanInput(input: string): string[] {
     const cleaned = input.trim().toLowerCase().split(/\s+/);
     return cleaned;
 }
+
+import { createInterface } from "node:readline";
+
+const replInterface = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Pokedex:> "
+});
+
+export function startREPL() {
+    replInterface.prompt();
+    replInterface.on("line", (line) => {
+        const cleanedInput = cleanInput(line);
+        if (cleanedInput.length === 0) {
+            replInterface.prompt();
+            return;
+        }
+        const command = cleanedInput[0];
+        console.log(`Your command was: ${command}`);
+        replInterface.prompt();
+    });
+}
